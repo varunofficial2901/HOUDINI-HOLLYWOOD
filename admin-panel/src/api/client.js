f0679899fd484a79d0f6b46aac3b5713cb672166
Varun Chauhan <varunofficial2901@gmail.com>
2026-04-11 01:12:29 +0530

    add admin panel ready for deployment

--- admin-panel/src/api/client.js
+++ admin-panel/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api", headers: { "Content-Type": "application/json" } });
+const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:8000"}/api`, headers: { "Content-Type": "application/json" } });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
